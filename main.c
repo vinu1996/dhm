6af78473abe3f4131004ad23727a8224288a54ce
vinu1996 <36875903+vinu1996@users.noreply.github.com>
2018-02-27 12:03:57 +0530

    Bugs fixed -- sharon
    bugs by vinayak are now fixed

--- main.c
+++ main.c
@@ -16,5 +16,19 @@
 +  {
 +    gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
 +  }
-
++  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
++  {
++    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
++    Serial.println("Satellite Count:");
++    Serial.println(gps.satellites.value());
++    Serial.println("Latitude:");
++    Serial.println(gps.location.lat(), 6);
++    Serial.println("Longitude:");
++    Serial.println(gps.location.lng(), 6);
++    Serial.println("Speed MPH:");
++    Serial.println(gps.speed.mph());
++    Serial.println("Altitude Feet:");
++    Serial.println(gps.altitude.feet());
++    Serial.println("");
++  }
 +}
